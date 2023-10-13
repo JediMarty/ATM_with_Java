fffae0c4f7b1a286b5daffa80c1f3bb9b156baec
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-10-13 20:18:32 +0300

    Add files via upload

---
+++ ATM_Курсов_Проект_Мартин_Петков/src/Atm/Virtual_ATM.java
@@ -0,0 +1,15 @@
+package Atm;
+
+class Virtual_ATM {
+	
+	private String pincode;
+	
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+		}
+	
+	public String getPincode() {
+    	return pincode;
+    }
+
+}
