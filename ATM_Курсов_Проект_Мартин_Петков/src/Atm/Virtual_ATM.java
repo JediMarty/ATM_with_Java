2bea241fbfd7d5fbd87568188853f78f79c57cde
JediMarty <130549056+JediMarty@users.noreply.github.com>
2023-10-19 20:01:47 +0300

    Update Virtual_ATM.java

--- ATM_Курсов_Проект_Мартин_Петков/src/Atm/Virtual_ATM.java
+++ ATM_Курсов_Проект_Мартин_Петков/src/Atm/Virtual_ATM.java
@@ -1,5 +1,13 @@
 package Atm;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 class Virtual_ATM {
 	
 	private String pincode;
@@ -14,7 +22,83 @@ class Virtual_ATM {
 	
 	public Boolean pinv(String enter,Boolean card) {
 	
+		 try {
+	            File file = new File("c:/Users/Admin/Desktop/gits/ATM/notess.txt");
+	            Scanner scanner = new Scanner(file);
+	            while(scanner.hasNext()) {
+	            if (enter.equals(scanner.next())) {
+	        		pincode = enter;
+	        		card = true;
+	        	    break;
+	        	}
+	           
+	            else if (enter.equals(scanner.next())) {
+	            	pincode = enter;
+	        		card = true;
+	        	    break;
+	            }
+	            
+	        	else {
+	        		System.out.println("!");
+	        		
+	        	    
+	        	}
+	            }  
+		 } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        }
+	    
+		 return card;
+		
+	
 	
 	}
-
+	 public int read_note(ATM person1,int money) {
+	        try {
+	            File file = new File("c:/Users/Admin/Desktop/gits/ATM/notes.txt");
+	            Scanner scanner = new Scanner(file);
+	        
+	            if(scanner.next().equals(pincode)) {
+	            
+	            	 money = scanner.nextInt();
+	            }
+	            else {
+	            	scanner.next();
+	            	scanner.next();
+	            	money = scanner.nextInt();}
+	            return money;
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        }
+	        return 0;
+	    }
+	 
+	   public void saves_note( ATM person1) {
+		   String s = ""; 
+		   int m = 0;
+		   try {
+			    BufferedReader reader = new BufferedReader(new FileReader("c:/Users/Admin/Desktop/gits/ATM/notes.txt"));
+	        	File file = new File("c:/Users/Admin/Desktop/gits/ATM/notes.txt");
+	        	Scanner scanner = new Scanner(file);
+	        	if(!(scanner.next().equals(pincode))) {
+	        		
+	        		
+	        		 s = reader.readLine();
+	            	
+	            }
+	        	else {
+	        		 reader.readLine();
+	        		 s = reader.readLine();
+	            
+	        	}
+	        	FileWriter filew = new FileWriter("c:/Users/Admin/Desktop/gits/ATM/notes.txt");
+	            
+	            filew.write(person1.getPincode() + " " + person1.getMoney() + "\n");
+	            filew.write(s);
+	            filew.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
+
